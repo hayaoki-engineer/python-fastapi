709eaccca09e66522589908d7aea4dfdc62bafc4
Taisei <taisei.960420@gmail.com>
2025-02-15 17:33:27 +0900

    追加

--- type_hints.py
+++ type_hints.py
@@ -1,8 +1,4 @@
-price: int = 100.1
-tax: float = 10
+from typing import List, Dict
 
-def calc_price__including_tax(price: int, tax: float) -> int:
-  return int(price * tax)
-
-if __name__ == '__main__':
-  print(f'{calc_price__including_tax(price, tax)}円')
+sample_list: List[int] = [1, 2, 3, 4]
+sample_dict: Dict[str, str] = {'key1': 'value1'}
