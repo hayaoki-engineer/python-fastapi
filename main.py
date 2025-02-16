18aa5b0705ef0bbf1a93916a94330d501beb9277
Taisei <taisei.960420@gmail.com>
2025-02-16 09:42:41 +0900

    クエリパラメータ

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/countries/japan")
-async def country():
-  return { "message": "This is Japan" }
+
 
 @app.get("/countries/{country_name}")
-async def country(country_name: str):
-  return { "country_name": country_name }
+async def country(country_name: str = "japan", city_name: str = "tokyo"  ):
+  return { 
+    "country_name": country_name, 
+    "city_name": city_name 
+  }
